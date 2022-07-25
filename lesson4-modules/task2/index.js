4a486524eb421794071df3982d54351aa0fe6f72
Sergi0let <s.vashkevych@gmail.com>
2022-07-25 12:19:15 +0300

    init

---
+++ lesson4-modules/task2/index.js
@@ -0,0 +1,18 @@
+const store = (function () {
+  let storage = {};
+
+  return {
+    addItem: function (key, value) {
+      Object.assign(storage, { [key]: value });
+    },
+    clear: function () {
+      storage = {};
+    },
+  };
+})();
+
+(function func() {
+  let a = 17;
+
+  console.log(a);
+})();
